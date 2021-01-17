5a4681f4312b42cd0c17ad8636810a9cbdf4f544
aryanpatel12 <patel3aryan@gmail.com>
2021-01-17 08:51:49 -0600

    added proper form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,81 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import MemberCard from './MemberCard';
-import ProfileCard from './ProfileCard'; 
-
-
+import ProfileCard from './ProfileCard';
 
 const useStyles = makeStyles((theme) => ({
-    dashLanding: {
-        height: '300px',
-        backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/raphael-stager-MPAFS1K7oYE-unsplash.jpg'})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100% 300px',
-        // verticalAlign : 'middle'
-        alignItems: 'center'
-    },
-    dashLandingContent: {
-        textAlign: 'center',
-        fontSize: '1.5rem',
-        fontFamily: 'Roboto Slab',
-        verticalAlign: 'middle', 
-        alignItems: 'center', 
-        paddingTop: '100px'
-    },
-    root: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh',
-        color: '#000',
-        fontFamily: 'Roboto Slab'
-    },
-    content: {
-        width: '70%',
-        margin: '0 auto',
-
-    },
-    moveDown: {
-        color: 'rgb(0,0,0)',
-        fontSize: '3rem'
-    },
+	dashLanding: {
+		height: '300px',
+		backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/raphael-stager-MPAFS1K7oYE-unsplash.jpg'})`,
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: '100% 300px',
+		// verticalAlign : 'middle'
+		alignItems: 'center',
+	},
+	dashLandingContent: {
+		textAlign: 'center',
+		fontSize: '1.5rem',
+		fontFamily: 'Roboto Slab',
+		verticalAlign: 'middle',
+		alignItems: 'center',
+		paddingTop: '100px',
+	},
+	root: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '90vh',
+		color: '#000',
+		fontFamily: 'Roboto Slab',
+	},
+	content: {
+		width: '70%',
+		margin: '0 auto',
+	},
+	moveDown: {
+		color: 'rgb(0,0,0)',
+		fontSize: '3rem',
+	},
 }));
 
 function Dashboard() {
-    const classes = useStyles();
-    const user = {"name": "Sample User", "email" : "sampleUser@gmail.com", "subgroup": "2", "members" : "['sampleuser2@gmail.com, 'sampleuser3@gmail.com']"}
+	const classes = useStyles();
+	const user = {
+		name: 'Sample User',
+		email: 'sampleUser@gmail.com',
+		subgroup: '2',
+		members: "['sampleuser2@gmail.com, 'sampleuser3@gmail.com']",
+	};
 
-    return (
-        <div>
-            <CssBaseline />
-            <div className={classes.dashLanding}>
-                <h1 className={classes.dashLandingContent}>Welcome To Connect2 ! </h1>
-            </div>
-            <div className = {classes.content}>
-                <Grid container spacing={2} p={2}>
-                    <Grid item xs ={4}>
-                        <p >GROUP MEMBERS</p>
-                        <MemberCard m={2}/>
-                        <MemberCard m={2}/>
-                        <MemberCard m={2}/>
-
-                    </Grid>
-                    <Grid item xs ={4}>
-                    <p>PROFILE CARD </p>
-                        <ProfileCard user = {user} />
-                    </Grid>
-                    <Grid item xs ={4}>
-                    <p> MEETINGS </p>
-                    <a href="www.zoom.com//"> Upcoming Zoom Meeting </a>
-                    </Grid>
-                </Grid>
-            </div>            
-        </div>
-    )
-
+	return (
+		<div>
+			<CssBaseline />
+			<div className={classes.dashLanding}>
+				<h1 className={classes.dashLandingContent}>Welcome To Connect2 ! </h1>
+			</div>
+			<div className={classes.content}>
+				<Grid container spacing={2} p={2}>
+					<Grid item xs={4}>
+						<p>GROUP MEMBERS</p>
+						<MemberCard m={2} />
+						<MemberCard m={2} />
+						<MemberCard m={2} />
+					</Grid>
+					<Grid item xs={4}>
+						<p>PROFILE CARD </p>
+						<ProfileCard user={user} />
+					</Grid>
+					<Grid item xs={4}>
+						<p> MEETINGS </p>
+						<p>
+							4:30 PM
+							<a href="www.zoom.com//"> Upcoming Zoom Meeting </a>
+						</p>
+					</Grid>
+				</Grid>
+			</div>
+		</div>
+	);
 }
 
-
-export default Dashboard; 
+export default Dashboard;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,149 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
-
+import { CircularProgress, Container, Typography, Button } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import Question from './Question';
+import { db, firebaseAppAuth } from '../firebase';
+import axios from 'axios';
+import Auth from '../core';
+// import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-
+	root: {
+		height: '100%',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	type: {
+		padding: '20px 0 0 0',
+		fontFamily: 'system-ui',
+		color: 'darkturquoise',
+	},
+	button: {
+		margin: '0 0 50px 0',
+	},
+	center: {
+		display: 'flex',
+		justifyContent: 'center',
+	},
 }));
 
-const Form = ({inputText, setInputText, courses, setCourses})=>{
-  const classes = useStyles();
-  const inputTextHandler = (e) => {
-    console.log('TARGET')
-    console.log(e.target.value); 
-    setInputText(e.target.value); 
-    console.log("INPUT TEXT")
-    console.log(inputText)
-  }; 
+function Form(props) {
+	const classes = useStyles();
+	// const history = useHistory();
+	const [loading, setLoading] = useState(true);
+	const [questions, setQuestions] = useState([]);
+	const [answers, setAnswers] = useState(new Array(50).fill(5));
 
-  const submitCourseHandler = (e) => {
-    e.preventDefault(); 
-    setCourses([
-      ...courses, {text:inputText, id: Math.random()*1000}
-    ]); 
-    setInputText("");
-  }
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await db.collection('questions').doc('67kBGHf5bDg45ZLm0Vs7').get();
 
-  return (
-      <div>
-          <h1>{inputText}</h1>
-          <form>
-            <input value = {inputText}onChange={inputTextHandler} type="text" className='courseInput'/> 
-            <button onClick={submitCourseHandler} className="addCourseBtn" type="submit">
-            Submit 
-            </button>
-          </form>
-        
-      </div>
-  );
+				let data = ['Not found'];
+
+				if (res.exists) {
+					data = res.data().questions;
+				}
+
+				setQuestions(data);
+				setLoading(false);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	const onSubmit = (context) => {
+		// const { history } = this.props;
+		setLoading(true);
+		console.log('submit');
+		console.log(context);
+		const axiosInstance = axios.create({
+			baseURL: 'http://localhost:5000/connect-to-d2e1e/us-central1',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+				// Authorization: `Bearer TOEKN FOES HERER`,
+			},
+		});
+		const arrOfAnswers = [answers];
+		console.log(arrOfAnswers);
+		axiosInstance
+			.post('/predictPersonalityGroup', { instances: arrOfAnswers })
+			.then(async function (data) {
+				console.log(data);
+				const group = data.data.predictions[0];
+				console.log(group);
+				console.log('success');
+				console.log(firebaseAppAuth.currentUser.email);
+				await db
+					.collection('users')
+					.where('email', '==', firebaseAppAuth.currentUser.email)
+					.get()
+					.then((snap) =>
+						snap.forEach((doc) => {
+							doc.ref.update({ personalityGroup: group });
+						}),
+					);
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+		props.history.push('/app/dashboard');
+	};
+
+	const handleChange = (answer, index) => {
+		let newArr = [...answers];
+		newArr[index] = answer;
+		setAnswers(newArr);
+	};
+
+	return (
+		<Container className={loading ? classes.root : ''}>
+			{loading ? (
+				<Container className={classes.root}>
+					<CircularProgress />
+				</Container>
+			) : (
+				<div>
+					<Typography variant="h3" align="center" className={classes.type}>
+						Fill out this form to get matched!
+					</Typography>
+					{questions.map((question, key) => {
+						return (
+							<Question
+								handleChange={handleChange}
+								question={question}
+								key={key}
+								index={key}
+								answer={answers[key]}
+							></Question>
+						);
+					})}
+					<div className={classes.center}>
+						<Auth.Consumer>
+							{(context) => {
+								return (
+									<Button
+										variant="contained"
+										color="primary"
+										className={classes.button}
+										endIcon={<SendIcon />}
+										onClick={() => onSubmit(context)}
+									>
+										Submit
+									</Button>
+								);
+							}}
+						</Auth.Consumer>
+					</div>
+				</div>
+			)}
+		</Container>
+	);
 }
 export default Form;
